f0f0f3fcdca4cff6e66186bd33c421ce132cf61e
Juan Felipe Cárdenas García <juanfelipelo64@gmail.com>
2026-09-18 01:52:23 -0500

    feat: conectar PostgreSQL con SQLAlchemy

---
+++ app/db/session.py
@@ -0,0 +1,24 @@
+from sqlalchemy import URL, create_engine
+from sqlalchemy.orm import sessionmaker
+from app.core.config import settings
+
+
+database_url = URL.create(
+    drivername="postgresql+psycopg",
+    username=settings.database_user,
+    password=settings.database_password,
+    host=settings.database_host,
+    port=settings.database_port,
+    database=settings.database_name,
+)
+
+engine = create_engine(
+    database_url,
+    pool_pre_ping=True,
+)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    expire_on_commit=False,
+)
